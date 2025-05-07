1e75df8cabc20bbda0d55009141f215d672174d9
Sergey Serov <s.serov@echoguardian.ru>
2025-05-07 15:17:19 +0300

    Update mail.py

--- analyzers/mail.py
+++ analyzers/mail.py
@@ -1,13 +1,21 @@
 import mailparser
+from bs4 import BeautifulSoup
+
+def extract_urls_from_html(html_body):
+    soup = BeautifulSoup(html_body, 'html.parser')
+    return [a['href'] for a in soup.find_all('a', href=True)]
 
 def parse_email(eml_path):
     mail = mailparser.parse_from_file(eml_path)
+    html_urls = extract_urls_from_html(mail.body)
+    combined_urls = list(set(mail.urls + html_urls))  # удалим дубликаты, если есть
+
     return {
         'subject': mail.subject,
         'from': mail.from_,
         'to': mail.to,
         'date': mail.date,
         'body': mail.body,
-        'urls': mail.urls,
+        'urls': combined_urls,
         'attachments': mail.attachments,
     }

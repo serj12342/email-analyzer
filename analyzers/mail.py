a793ee07e142e2e494dce0358e5e9b302be6a286
Sergey Serov <s.serov@echoguardian.ru>
2025-05-07 15:18:21 +0300

    Update mail.py

--- analyzers/mail.py
+++ analyzers/mail.py
@@ -10,10 +10,14 @@ def parse_email(eml_path):
     html_urls = extract_urls_from_html(mail.body)
     combined_urls = list(set(mail.urls + html_urls))  # удалим дубликаты, если есть
 
+    # Преобразуем from/to в строки
+    from_str = ", ".join(email for name, email in mail.from_)
+    to_str = ", ".join(email for name, email in mail.to)
+
     return {
         'subject': mail.subject,
-        'from': mail.from_,
-        'to': mail.to,
+        'from': from_str,
+        'to': to_str,
         'date': mail.date,
         'body': mail.body,
         'urls': combined_urls,

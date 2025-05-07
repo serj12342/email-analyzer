5747d3fde9ff1c609b224286e6121a30b1c050eb
Sergey Serov <s.serov@echoguardian.ru>
2025-05-07 15:22:19 +0300

    Update mail.py

--- analyzers/mail.py
+++ analyzers/mail.py
@@ -5,22 +5,23 @@ def extract_urls_from_html(html_body):
     soup = BeautifulSoup(html_body, 'html.parser')
     return [a['href'] for a in soup.find_all('a', href=True)]
 
-def format_addresses(addr_list):
-    # addr_list выглядит как: [(name, email), ...]
+def format_addresses(address_list):
+    # [(None, 'example@mail.com')] или [('John', 'john@mail.com')]
     return ", ".join(
-        [f"{name} <{email}>" if name else email for name, email in addr_list]
+        f"{name} <{email}>" if name else email
+        for name, email in address_list
     )
 
 def parse_email(eml_path):
     mail = mailparser.parse_from_file(eml_path)
     html_urls = extract_urls_from_html(mail.body)
-    combined_urls = list(set(mail.urls + html_urls))  # удалим дубликаты
+    combined_urls = list(set(mail.urls + html_urls))
 
     return {
         'subject': mail.subject,
         'from': format_addresses(mail.from_),
         'to': format_addresses(mail.to),
-        'date': mail.date,
+        'date': str(mail.date),
         'body': mail.body,
         'urls': combined_urls,
         'attachments': mail.attachments,
